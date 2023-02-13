d29a53321ab2c22995529d4040983f3f7a63f39a
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-13 23:55:11 +0530

    fully refactoured Customer Register Form..

--- Car_Rental-Front_End/controllers/NavigationController.js
+++ Car_Rental-Front_End/controllers/NavigationController.js
@@ -1,3 +1,23 @@
+
 $("#btnOpenRegisterForm").click(function () {
     openRegisterForm();
-});
+})
+
+function openRegisterForm(){
+/*    generateRegisterIds();
+    generateUserIds();*/
+
+    $('#RegisterPage').css('display','block');
+
+    $("#nav_bar").css('display','block');
+    $("#banner").css('display','none');
+    $("#services").css('display','none');
+    $("#about_us").css('display','none');
+    $("#featured-car").css('display','none');
+    $("#testimonials").css('display','none');
+    $("#Contact_Page").css('display','none');
+    $("#social_media").css('display','none');
+    $("#footer").css('display','none');
+
+
+}
