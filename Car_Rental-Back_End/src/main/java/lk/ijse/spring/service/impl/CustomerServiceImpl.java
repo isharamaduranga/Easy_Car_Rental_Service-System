493f164db745115d24a133ff134a96e7d784da57
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-14 15:05:13 +0530

    Created CustomerService Interface and CustomerServiceImpl classes and implement it..

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,14 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/14/2023
+ * Time        : 3:03 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.service.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+}
