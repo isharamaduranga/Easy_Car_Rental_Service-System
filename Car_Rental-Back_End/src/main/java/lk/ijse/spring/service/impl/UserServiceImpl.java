32934745055a88cf224e1e0b870d609f013cd5a8
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-14 15:08:02 +0530

    Created UserService Interface and UserServiceImpl classes and implement it..

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/14/2023
+ * Time        : 3:06 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+}
