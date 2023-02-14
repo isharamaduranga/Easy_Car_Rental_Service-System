f1e14a05dba4e75359b9d184b71ba52ab5f771d4
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-14 14:34:41 +0530

    Created UsersDTO and Apply lombok annotatioons..

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/dto/UsersDTO.java
@@ -0,0 +1,24 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/14/2023
+ * Time        : 2:33 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class UsersDTO {
+    private String userId;
+    private String username;
+    private String password;
+}
