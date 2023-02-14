e9a7ced475cc25d4e64474ee149fee16008dc4f9
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-14 11:07:47 +0530

    Fully Refactoured All Driver and Admin Login Form and applied Styles..

--- Car_Rental-Front_End/controllers/NavigationController.js
+++ Car_Rental-Front_End/controllers/NavigationController.js
@@ -1,14 +1,79 @@
 
-$("#btnOpenRegisterForm").click(function () {
+$("#btnOpenRegisterForm,#btnRegisterInLoginPage").click(function () {
     openRegisterForm();
-})
-
-
+});
 
 $("#btnOpenDriver_Sign_Form").click(function () {
     openDriverForm();
-})
+});
 
+$("#btnOpenCusLoginForm").click(function () {
+    openCusLoginForm();
+});
+
+
+$("#btnOpenDriverLoginForm").click(function () {
+    openDriverLoginForm();
+});
+
+
+$("#btnOpenAdminLoginForm").click(function () {
+    openAdminLoginForm();
+});
+
+function openAdminLoginForm() {
+    $("#nav_bar").css('display','block');
+    $("#logInAdmin").css('display','block');
+
+    $("#logInDriver").css('display','none');
+    $('#RegisterPage').css('display','none');
+    $('#logInCustomer').css('display','none');
+    $("#banner").css('display','none');
+    $("#services").css('display','none');
+    $("#about_us").css('display','none');
+    $("#featured-car").css('display','none');
+    $("#testimonials").css('display','none');
+    $("#Contact_Page").css('display','none');
+    $("#social_media").css('display','none');
+    $("#footer").css('display','none');
+}
+
+
+function openDriverLoginForm() {
+
+    $("#nav_bar").css('display','block');
+    $("#logInDriver").css('display','block');
+
+    $('#RegisterPage').css('display','none');
+    $('#logInCustomer').css('display','none');
+    $("#banner").css('display','none');
+    $("#services").css('display','none');
+    $("#about_us").css('display','none');
+    $("#featured-car").css('display','none');
+    $("#testimonials").css('display','none');
+    $("#Contact_Page").css('display','none');
+    $("#social_media").css('display','none');
+    $("#footer").css('display','none');
+}
+
+
+
+function openCusLoginForm() {
+
+    $('#logInCustomer').css('display','block');
+    $("#nav_bar").css('display','block');
+
+    $('#RegisterPage').css('display','none');
+
+    $("#banner").css('display','none');
+    $("#services").css('display','none');
+    $("#about_us").css('display','none');
+    $("#featured-car").css('display','none');
+    $("#testimonials").css('display','none');
+    $("#Contact_Page").css('display','none');
+    $("#social_media").css('display','none');
+    $("#footer").css('display','none');
+}
 
 
 function openRegisterForm(){
@@ -16,6 +81,8 @@ function openRegisterForm(){
     generateUserIds();*/
 
     $('#RegisterPage').css('display','block');
+
+    $('#logInCustomer').css('display','none');
 
     $("#nav_bar").css('display','block');
     $("#banner").css('display','none');
@@ -28,18 +95,26 @@ function openRegisterForm(){
     $("#footer").css('display','none');
 }
 
-$("#btnRegisterCancel").click(function () {
+$("#btnRegisterCancel,#btnCancelToSystem,#btnCancelToDLogin,#btnCancelToAdmin").click(function () {
   registerCancelEvent();
 });
 
 $("#btnCancelDriver").click(function () {
   registerCancelEvent();
 });
+$("#btnCancelToAdmin").click(function () {
+  registerCancelEvent();
+});
+
+
 
 
 function registerCancelEvent() {
     $('#RegisterPage').css('display','none');
     $('#RegisterDriver').css('display','none');
+    $('#logInCustomer').css('display','none');
+    $('#logInDriver').css('display','none');
+    $('#logInAdmin').css('display','none');
 
     $("#nav_bar").css('display','block');
     $("#banner").css('display','block');
