7dd0ce9cfdef3b1a03f4aa0b0d83369397dabae3
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-17 19:17:51 +0530

    Created CarService Interface with Back end dto pkg....

--- Car_Rental-Back_End/src/main/java/lk/ijse/spring/service/CarService.java
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/service/CarService.java
@@ -5,5 +5,15 @@ import lk.ijse.spring.dto.CarDTO;
 import java.util.List;
 
 public interface CarService {
+    void saveCar(CarDTO car);
 
+    void updateCar(CarDTO car);
+
+    void deleteCar(String id);
+
+    CarDTO searchCar(String id);
+
+    List<CarDTO> getAllCars();
+
+    String generateCarIds();
 }
