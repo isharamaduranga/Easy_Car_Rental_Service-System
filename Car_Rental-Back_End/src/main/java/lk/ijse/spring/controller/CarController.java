12e5e2abed087be5ed934e06137986a202911e56
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-17 20:50:21 +0530

    Content Updated CarController with Back end impl pkg...

--- Car_Rental-Back_End/src/main/java/lk/ijse/spring/controller/CarController.java
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/controller/CarController.java
@@ -8,15 +8,51 @@
 
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.spring.dto.CarDTO;
+import lk.ijse.spring.service.CarService;
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("car")
 @CrossOrigin
 public class CarController {
+    @Autowired
+    CarService carService;
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil getAllCar() {
+        return new ResponseUtil(200, "Ok", carService.getAllCars());
+    }
 
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil saveCar(@RequestBody CarDTO carDTO){
+        carService.saveCar(carDTO);
+        return new ResponseUtil(200, "Successfully Saved.",null);
+    }
+
+    @DeleteMapping(params = {"id"},produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil deleteCar(@RequestParam String id){
+        carService.deleteCar(id);
+        return new ResponseUtil(200, "Successfully Deleted.", null);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil updateCar(@RequestBody CarDTO carDTO){
+        carService.updateCar(carDTO);
+        return new ResponseUtil(200, "Successfully Updated.",null);
+    }
+
+    @GetMapping(path = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseUtil searchCar(@PathVariable("id") String id){
+        return new ResponseUtil(200, "Ok.",carService.searchCar(id));
+    }
+
+    @GetMapping(params = {"test"})
+    public ResponseUtil generateCarIds(@RequestParam String test) {
+        return new ResponseUtil(200, "Ok", carService.generateCarIds());
+    }
 
 }
