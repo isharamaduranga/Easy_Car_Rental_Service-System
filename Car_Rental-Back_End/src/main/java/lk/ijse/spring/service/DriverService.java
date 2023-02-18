fc874f3c8d2f73b2d3476109ae753091a136ff02
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-18 15:53:02 +0530

    Created driverService with Back end service pkg....

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/service/DriverService.java
@@ -0,0 +1,20 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.DriverDTO;
+
+import java.util.List;
+
+public interface DriverService {
+    void saveDriver(DriverDTO driver);
+
+    void updateDriver(DriverDTO driver);
+
+    void deleteDriver(String id);
+
+    DriverDTO searchDriver(String id);
+
+    List<DriverDTO> getAllDrivers();
+
+    String generateDriverIds();
+
+}
