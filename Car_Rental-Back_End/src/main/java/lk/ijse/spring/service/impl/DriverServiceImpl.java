84afbba0d854df1821a41f0d86dd9f1f864d22d9
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-18 15:55:06 +0530

    Created driverServiceImpl with Back end service pkg....

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/service/impl/DriverServiceImpl.java
@@ -0,0 +1,50 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/18/2023
+ * Time        : 3:53 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.dto.DriverDTO;
+import lk.ijse.spring.service.DriverService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public void saveDriver(DriverDTO driver) {
+
+    }
+
+    @Override
+    public void updateDriver(DriverDTO driver) {
+
+    }
+
+    @Override
+    public void deleteDriver(String id) {
+
+    }
+
+    @Override
+    public DriverDTO searchDriver(String id) {
+        return null;
+    }
+
+    @Override
+    public List<DriverDTO> getAllDrivers() {
+        return null;
+    }
+
+    @Override
+    public String generateDriverIds() {
+        return null;
+    }
+}
