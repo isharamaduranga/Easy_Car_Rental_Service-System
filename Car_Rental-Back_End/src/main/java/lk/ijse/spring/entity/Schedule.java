b1474800b1f0bacf90b90164e1d34ee35c8d2641
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-22 01:20:04 +0530

    Content Updated entity with backend and apply lambok...

--- Car_Rental-Back_End/src/main/java/lk/ijse/spring/entity/Schedule.java
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/entity/Schedule.java
@@ -8,10 +8,21 @@
 
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Schedule {
 
     @Id
