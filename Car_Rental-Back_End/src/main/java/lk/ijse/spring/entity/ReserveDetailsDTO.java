e91759f253c17c30775718adcd256c690a8326d8
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-22 00:53:00 +0530

    Created new Dto class ReserveDetailsDTO...

--- Car_Rental-Back_End/src/main/java/lk/ijse/spring/entity/ReserveDetailsDTO.java
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/entity/ReserveDetailsDTO.java
@@ -8,7 +8,25 @@
 
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class ReserveDetailsDTO {
 
-
+    private String reserveId;
+    private String carId;
+    private String type;
+    private String colour;
+    private String brand;
+    private String driverWantOrNot;
+    private String driverId;
+    private String driverName;
+    private String driverContact;
+    private double loseDamageWaiverPayment;
 }
