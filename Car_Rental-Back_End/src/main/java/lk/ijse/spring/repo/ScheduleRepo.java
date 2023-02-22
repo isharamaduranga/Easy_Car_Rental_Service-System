9ac03ae6ad4b5a6954b2df3c1fe0d94baf637bb0
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-22 11:03:44 +0530

    Created new Schedule Repo...

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/repo/ScheduleRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.spring.repo;
+
+import lk.ijse.spring.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepo extends JpaRepository<Schedule,String> {
+}
