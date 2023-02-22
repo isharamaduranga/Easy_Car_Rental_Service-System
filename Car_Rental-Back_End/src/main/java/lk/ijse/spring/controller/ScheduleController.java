2885011028067f0a2206c8f72fb3e91ab81a9ea2
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-22 11:13:16 +0530

    Created new class ScheduleController ...

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/controller/ScheduleController.java
@@ -0,0 +1,20 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/22/2023
+ * Time        : 11:12 AM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("schedule")
+@CrossOrigin
+public class ScheduleController {
+
+}
