ea41173a5449ea51f9a2cfa0a3ea68f7c4906a3c
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-22 11:10:55 +0530

    Created new class ReserveController..

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/controller/ReserveController.java
@@ -0,0 +1,23 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/22/2023
+ * Time        : 11:08 AM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("reserve")
+@CrossOrigin
+public class ReserveController {
+
+
+
+}
