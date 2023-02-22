807f4cdf587081e00d0d2c2736fd83b21a5b4386
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-22 11:22:04 +0530

    Created new class ScheduleService & which impl class..

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/service/impl/ScheduleServiceImpl.java
@@ -0,0 +1,27 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/22/2023
+ * Time        : 11:20 AM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.repo.ScheduleRepo;
+import lk.ijse.spring.service.ScheduleService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ScheduleServiceImpl implements ScheduleService {
+
+    @Autowired
+    ScheduleRepo scheduleRepo;
+
+    @Autowired
+    ModelMapper mapper;
+}
