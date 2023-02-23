f620fd2c7c2a7cd8d7669e46b4612144322a1fa5
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-23 20:07:39 +0530

    Implement function load payment details to table..

--- Car_Rental-Front_End/controllers/PaymentController.js
+++ Car_Rental-Front_End/controllers/PaymentController.js
@@ -159,7 +159,7 @@ function calculateIncome() {
     }
 
     $.ajax({
-        url: "http://localhost:8080/Car_Rental_System_war/payment",
+        url: baseURLForPayment+"payment",
         method: "POST",
         contentType: "application/json",
         data: JSON.stringify(paymentDetail),
@@ -170,6 +170,31 @@ function calculateIncome() {
                 loadPayments();
 
             }
+        },
+        error: function (ob) {
+            alert(ob.responseJSON.message);
+        }
+    });
+}
+
+function loadPayments() {
+    $.ajax({
+        url: baseURLForPayment+"payment",
+        method: "GET",
+        success: function (response) {
+
+            $("#tblPayment tbody").empty();
+            for (var responseKey of response.data) {
+                let raw = `<tr><td> ${responseKey.paymentId} </td><td> ${responseKey.paymentDate} </td><td> ${responseKey.rentFee} </td><td> ${responseKey.driverFee} </td>
+                               <td> ${responseKey.loseDamagePayment} </td><td> ${responseKey.reduceLoseDamagePayment} </td><td>
+                                <span class="badge badge-success rounded-pill d-inline">${responseKey.harmOrNot}</span></td> 
+                                <td>${responseKey.travelledDistance} </td><td> ${responseKey.extraKm} </td><td>${responseKey.extraKmPrice}</td><td>${responseKey.fullPayment}</td>
+                                <td><button type="button" class="btn btn-warning btn-sm px-3" data-ripple-color="dark">
+                                    <i class="fas fa-pen-alt"></i>
+                                </button></td></tr>`;
+                $("#tblPayment tbody").append(raw);
+            }
+            generatePaymentIds();
         },
         error: function (ob) {
             alert(ob.responseJSON.message);
