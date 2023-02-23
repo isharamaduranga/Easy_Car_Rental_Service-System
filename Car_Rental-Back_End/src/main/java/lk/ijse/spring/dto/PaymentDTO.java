6970edd37afe7fe8e629d837c8dc7bfe888cffee
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-23 19:34:36 +0530

    Created new DTO class PaymentDTO..

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/dto/PaymentDTO.java
@@ -0,0 +1,36 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/23/2023
+ * Time        : 7:32 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class PaymentDTO {
+
+    private String paymentId;
+    private String paymentDate;
+    private double rentFee;
+    private String harmOrNot;
+    private double loseDamagePayment;
+    private double reduceLoseDamagePayment;
+    private double driverFee;
+    private double travelledDistance;
+    private double extraKm;
+    private double extraKmPrice;
+    private double fullPayment;
+
+    private ReserveDetailsDTO reserveDetails;
+
+}
