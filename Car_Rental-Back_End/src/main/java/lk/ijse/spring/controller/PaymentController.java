05e27b261d1eebaaf1a07fde1d1938feb885c4b0
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-02-23 19:40:08 +0530

    Created new PaymentController class..

---
+++ Car_Rental-Back_End/src/main/java/lk/ijse/spring/controller/PaymentController.java
@@ -0,0 +1,20 @@
+/**
+ * @author : Ishara Maduarnga
+ * Project Name: Easy_Car_Rental_Service-System
+ * Date        : 2/23/2023
+ * Time        : 7:35 PM
+ * Year        : 2023
+ */
+
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("payment")
+@CrossOrigin
+public class PaymentController {
+
+}
